50cea95b3c75e4c4e52603eb6ad0000c47d63dc5
Yu KATO (JP) <yu.kato@monstar-lab.com>
2015-09-28 22:46:35 +0900

    delete log

--- index.js
+++ index.js
@@ -32,10 +32,6 @@ module.exports = function (opts) {
       return cb();
     }
 
-    gutil.log(file.path);
-    var originalPath = file.path;
-    gutil.log('original path : ', originalPath);
-
     if (options.jsonPath == null) {
       this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'json path is required'));
       return cb();
